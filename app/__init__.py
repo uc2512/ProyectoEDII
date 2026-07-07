2c6365b85fca6adca651509ca3ce89bde2aa3d0a
uc2512 <urielribera25@gmail.com>
2026-07-07 13:38:10 -0400

    p

--- app/__init__.py
+++ app/__init__.py
@@ -39,8 +39,11 @@ def crear_app():
     with app.app_context():
         db.create_all()
         from app.modelos import Usuario
-        if not Usuario.query.filter_by(usuario="admin").first():
-            nuevo = Usuario(usuario="admin", password="1234")
+        if not Usuario.query.filter_by(usuario="herboT2026").first():
+            nuevo = Usuario(
+                usuario="herboT2026",
+                password="uab2026CIS$"
+            )
             db.session.add(nuevo)
             db.session.commit()
 
