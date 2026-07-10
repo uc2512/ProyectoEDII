d36d015c7ee1f1712105a5694e14825193878e40
uc2512 <urielribera25@gmail.com>
2026-07-10 11:52:14 -0400

    64

--- app/modelos.py
+++ app/modelos.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from zoneinfo import ZoneInfo
 from flask_login import UserMixin
 
 class Paciente(db.Model):
@@ -7,14 +8,14 @@ class Paciente(db.Model):
     carnet      = db.Column(db.String(20), primary_key=True)
     nombre      = db.Column(db.String(100), nullable=False)
     edad        = db.Column(db.Integer)
-    contacto    = db.Column(db.String(20))
+    contacto    = db.Column(db.String(254))
     sesiones    = db.relationship('Sesion', backref='paciente', lazy=True, cascade='all, delete-orphan')
 
 class Sesion(db.Model):
     __tablename__ = 'sesiones'
     id           = db.Column(db.Integer, primary_key=True)
     carnet       = db.Column(db.String(20), db.ForeignKey('pacientes.carnet'), nullable=False)
-    fecha        = db.Column(db.DateTime, default=lambda: datetime.now())
+    fecha        = db.Column(db.DateTime, default=lambda: datetime.now(ZoneInfo("America/La_Paz")))
     diagnostico  = db.Column(db.Text)
     tratamiento  = db.Column(db.Text)
 
